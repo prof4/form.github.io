8df5f8a1c014a85b7e1aa0e779340fa7e8920371
prof4 <52273992+prof4@users.noreply.github.com>
2020-12-18 14:38:24 +0100

    h

---
+++ form_process.php
@@ -0,0 +1,74 @@
+<?php
+
+//define variables and set to empty values
+$name_error = $email_error = $phone_error = $url_error = "";
+$name = $email = $phone = $message = $url = $success= "";
+
+//form is submitted with POST method
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (empty($_POST["name"])){
+        $name_error= "Name is required";
+    } else{
+        $name = test_input($_POST["name"]);
+        // check if name only contains lettes and whitespace
+        if (!preg_match("/^[a-zA-Z ]*$/", $name)){
+            $name_error = "Only letters and whitespace allowed";
+        }
+    }
+    if (empty($_POST["email"])){
+        $email_error= "email is required";
+    } else{
+        $email = test_input($_POST["email"]);
+        // check if correct
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)){
+            $name_error = "Only letters and whitespace allowed";
+        }
+    }
+    if (empty($_POST["discord"])){
+        $discord_error= "dicordname#tag is required";
+    } else{
+        $discord = test_input($_POST["discord"]);
+        // check if correct
+        if (!preg_match("[a-z0-9]#[0-9]", $name)){
+            $discord_error = "invalid dicordname#tag";
+        }
+    }
+    if (empty($_POST["url"])){
+        $url_error= "";
+    } else{
+        $url = test_input($_POST["url"]);
+        // check if URL adress syntax is valid (this regular expression also allows dashes in the URL)
+        if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i", $url)){
+            $url_error = "Invalid URL";
+        }
+    }
+    if (empty($_POST["message"])){
+        $message= "";
+    } else{
+        $message = test_input($_POST["message"]);
+    }
+
+    if ($name_error == '' and $email_error == '' and $discord_error =='' and $url_error == ''){
+        $message_body = '';
+        unset($_POST['submit']);
+        foreach ($_POST as $key => $value){
+            $message_body .= "$key: $value\n";
+        }
+
+        $to = 'ultralgendsoul@gmail.com';
+        $subject = 'contact form submit';
+        if (mail($to, $subject, $message)){
+            $success = "Message sent, thank you for contacting us!";
+            $name_error = $email_error = $phone_error = $url_error = "";
+        }
+    }
+    
+}
+
+function test_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+?>
